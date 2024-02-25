3ae4c4e395b41bb8d6f0b23fd720cd82653cec19
Molemo Molefe <mmolefe@Molemos-iMac.local>
2024-02-25 16:49:02 +0200

    expanded site skeleton and added new pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('', views.greeting)
+    path('', views.greeting),
+    path('about/', views.about),
+    path('philosophy/', views.philosophy),
+    path('services/', views.services),
+    path('contact/', views.contact)
 ]
--- home/views.py
+++ home/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 # Create your views here.
+
+# Home Page View
 def greeting(request):
     return HttpResponse('Welcome to the MCS Homepage!')
+
+def about(request):
+    return HttpResponse('About Us:')
+
+def philosophy(request):
+    return HttpResponse('Our Philosophy:')
+
+def services(request):
+    return HttpResponse('Our Services:')
+
+def contact(request):
+    return HttpResponse('Contact Us:')
